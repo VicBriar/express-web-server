3a6696b2e6f1bbe9278dd79e95c115fdc981b0e7
Victoria McCrady <spartanginger@gmail.com>
2023-03-06 10:39:55 -0800

    assigment complete

--- server.js
+++ server.js
@@ -1,1 +1,27 @@
 
+const express = require('express')
+const app = express();
+const port = 3000;
+
+
+app.use(express.static('public'))
+
+app.listen(port, () => {
+    console.log(`server listening at  http://localhost:${port}`)
+})
+
+/* example of route & endpoints
+const express = require('express');
+
+const app = express();
+const port = 3000;
+
+app.get('/now', (request, response) => {
+    const date = new Date();
+    response.send(date);
+})
+
+app.listen(port, () => {
+    console.log(`Your server is listening on port http://localhost:${port}/now`);
+})
+*/
